7fe820499b951241d93eb5bace6825d66e285ce6
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-02-14 00:39:53 +0500

    updated main css and added js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const menuBtn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn__burger');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu(){
+    if(!showMenu){
+        hamburger.classList.add('open');
+    }else{
+        hamburger.classList.remove('open');
+    }
+    showMenu = !showMenu;
+}
